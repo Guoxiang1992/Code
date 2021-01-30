5b6221b3b33202104a261d3933d251399c38911d
ZGX1992 <1293384325@qq.com>
2021-01-30 12:53:04 +0800

    hommework

---
+++ testing/calc_case.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*- 
+# @File : calc_case.py
+class Calc:
+    def add(self,a,b):
+        return a+b
+    def sub(self,a,b):
+        return a-b
+    def mul(self,a,b):
+        return a*b
+    def div(self,a,b):
+        return a/b
+
